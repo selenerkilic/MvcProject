f1533af47baa8ee181460626591723d5fef3dad2
selenerkilic <selenerkilic@gmail.com>
2020-12-06 12:43:37 +0300

    MvcProject

---
+++ MvcStok/Controllers/YardimController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class YardimController : Controller
+    {
+        // GET: Yardim
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
